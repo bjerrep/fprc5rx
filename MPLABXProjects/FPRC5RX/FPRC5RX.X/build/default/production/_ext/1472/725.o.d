4df5a7250a8ad9b4610df45cd0d5241977d24ed5
X3N0N <fusionphenom@gmail.com>
2020-12-16 23:42:35 +0100

    Added Ready2Go MPLAB X Project

---
+++ MPLABXProjects/FPRC5RX/FPRC5RX.X/build/default/production/_ext/1472/725.o.d
@@ -0,0 +1,14 @@
+build/default/production/_ext/1472/725.o:  \
+	C:/FPRC5RX/725.asm  \
+	C:/FPRC5RX/P16F873.inc  \
+	C:/FPRC5RX/PICDELAY.inc  \
+	C:/FPRC5RX/COMMON.inc  \
+	C:/FPRC5RX/16F873.inc  \
+	C:/FPRC5RX/RC5.asm  \
+	C:/FPRC5RX/I2C_DEFS.inc  \
+	C:/FPRC5RX/PICFLASH.inc  \
+	C:/FPRC5RX/AUTOCALC.inc  \
+	C:/FPRC5RX/I2C.inc  \
+	C:/FPRC5RX/PICEEPROM.INC  \
+	C:/FPRC5RX/RC5UTIL.asm  \
+
---
+++ MPLABXProjects/FPRC5RX/Readme.md
@@ -0,0 +1,5 @@
+Project compiled in "MPLAB X IDE v5.35" last version which use mpasm (aka mpasmx)
+HEX file can be found in "FPRC5RX.X\dist\default\production\FPRC5RX.X.production.hex"
+
+Note:
+To use this project in version higher than "MPLAB X IDE v5.35" must to be converted for XC8 COMPILER
